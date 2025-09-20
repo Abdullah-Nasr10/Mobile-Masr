e717af7be05c146aef59cfde4182d72f7d76b0ed
Gehad Sweeney <gehadsowany2001@gmail.com>
2025-09-20 03:43:06 +0200

    work on the card and everything depends on it.

--- Pages/Home/Home.jsx
+++ Pages/Home/Home.jsx
@@ -1,6 +1,20 @@
+import React from "react";
 import "./Home.css";
+import { products } from "../../components/ApiProducts/Apiproducts.js";
+import ProductsSlider from "../../components/ProductsSlider/ProductsSlider.jsx";
+
 function Home() {
-  return <div>Home</div>;
+  const geh_newProducts = products.filter((p) => p.status === "New");
+  const geh_usedProducts = products.filter((p) => p.status === "Used");
+
+  return (
+    <div className="container mt-5">
+      <ProductsSlider title="All Products" products={products} />
+      <ProductsSlider title="New Products" products={geh_newProducts} />
+      <ProductsSlider title="Used Products" products={geh_usedProducts} />
+    </div>
+  );
 }
 
 export default Home;
+
