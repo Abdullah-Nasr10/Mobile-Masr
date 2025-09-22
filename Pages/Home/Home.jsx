a665f2b9fe4978e06b74639f025497810d3096d9
rehababdelmonem <rehababdelmonem74@gmail.com>
2025-09-22 09:12:07 +0300

    reh:added header and icon

--- Pages/Home/Home.jsx
+++ Pages/Home/Home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./Home.css";
 import { products } from "../../components/ApiProducts/Apiproducts.js";
 import ProductsSlider from "../../components/ProductsSlider/ProductsSlider.jsx";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 function Home() {
   const items = [
     {
@@ -47,6 +50,155 @@ function Home() {
 
   return (
     <>
+      {/* =================================================== */}
+      <div className="container">
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          pagination={{ dynamicBullets: true }}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          loop={true}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              src="https://static.mobilemasr.com/public/settings/banners/68cd88d208d7f_1758300370.webp"
+              alt="Slide 1"
+              class="w-100"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://static.mobilemasr.com/public/settings/banners/68cd88d232710_1758300370.webp"
+              alt="Slide 2 "
+              class="w-100"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://static.mobilemasr.com/public/settings/banners/68cd88d24e72b_1758300370.webp"
+              alt="Slide 3 "
+              class="w-100"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      {/*icon header  */}
+      <div
+        className="container my-4 d-flex gap-3 p-3"
+        style={{
+          width: "1400px",
+          minHeight: "30px",
+          border: "2px solid #f5f2f2ff",
+          backgroundColor: "#fff",
+        }}
+      >
+        <div className="card p-2 flex-fill">
+          <div className="d-flex align-items-center">
+            <img
+              src="https://mobilemasr.com/_ipx/_/Images/feature/diagnostic_tool_new.svg"
+              alt="icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                marginRight: "10px",
+
+                padding: "3px",
+              }}
+            />
+            <div>
+              <p className="fw-bold mb-1" style={{ fontSize: "14px" }}>
+                Diagnostic Tool Report
+              </p>
+              <p className="mb-0" style={{ fontSize: "11px" }}>
+                Generate a diagnostic report for your device
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="card p-2 flex-fill"
+          style={{ borderLeft: "1px solid #fff" }}
+        >
+          <div className="d-flex align-items-center">
+            <img
+              src="https://mobilemasr.com/_ipx/_/Images/feature/secure_selling_new.svg"
+              alt="icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                marginRight: "10px",
+
+                padding: "3px",
+              }}
+            />
+            <div>
+              <p className="fw-bold mb-1" style={{ fontSize: "14px" }}>
+                Guaranteed Sellers & Buyers
+              </p>
+              <p className="mb-0" style={{ fontSize: "11px" }}>
+                Secure Platform for your info and cards
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="card p-2 flex-fill"
+          style={{ borderLeft: "1px solid #fff" }}
+        >
+          <div className="d-flex align-items-center">
+            <img
+              src="https://mobilemasr.com/_ipx/_/Images/feature/warranty_30_day_new.svg"
+              alt="icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                marginRight: "10px",
+
+                padding: "3px",
+              }}
+            />
+            <div>
+              <p className="fw-bold mb-1" style={{ fontSize: "14px" }}>
+                30 days warranty
+              </p>
+              <p className="mb-0" style={{ fontSize: "11px" }}>
+                Buy with warranty from Verified Stores
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="card p-2 flex-fill"
+          style={{ borderLeft: "1px solid #fff" }}
+        >
+          <div className="d-flex align-items-center">
+            <img
+              src="https://mobilemasr.com/_ipx/_/Images/feature/fast_delivery_new.svg"
+              alt="icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                marginRight: "10px",
+
+                padding: "3px",
+              }}
+            />
+            <div>
+              <p className="fw-bold mb-1" style={{ fontSize: "14px" }}>
+                Fast Delivery
+              </p>
+              <p className="mb-0" style={{ fontSize: "11px" }}>
+                Safe Delivery to All Governorates
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* ================================================== */}
       <div>
         <div className="heb-categories-container">
           {items.map((it) => (
