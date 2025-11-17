b46bb4538b25f64d60c1a19d971d2f2a3938d67a
Abdullah Nasr <abdullah.nasr1022@gmail.com>
2025-11-17 20:33:25 +0200

    abd: solve error

--- src/components/HomeComponents/CategorySlider/CategorySlider.jsx
+++ src/components/HomeComponents/CategorySlider/CategorySlider.jsx
@@ -5,12 +5,18 @@ import "swiper/css";
 import { Autoplay } from "swiper/modules";
 import axios from "axios";
 import "./CategorySlider.css";
-import { Link } from "react-router-dom";
 
 const CategorySlider = () => {
   const [categories, setCategories] = useState([]);
 
-  const CatName = ["mobile", "tablet", "smart-watches", "wireless-earbuds", "game-consoles","laptop"];
+  const CatName = [
+    "mobile",
+    "tablet",
+    "smart-watches",
+    "wireless-earbuds",
+    "game-consoles",
+    "laptop",
+  ];
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -26,38 +32,39 @@ const CategorySlider = () => {
 
   return (
     <div className="geh-category-slider container">
-<Swiper
-  modules={[Autoplay]}
-  autoplay={{ delay: 2000, disableOnInteraction: false }}
-  spaceBetween={32} 
-  slidesPerView={6}
-  breakpoints={{
-    320: { 
-      slidesPerView: 2,
-      spaceBetween: 10
-    },
-    480: { 
-      slidesPerView: 3,
-      spaceBetween: 20
-    },
-    768: { 
-      slidesPerView: 4,
-      spaceBetween: 22 
-    },
-    1024: { 
-      slidesPerView: 4,
-      
-    },
-    1280: { 
-      slidesPerView: 6,
-      spaceBetween: 32
-    },
-  }}
->
-
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        spaceBetween={32}
+        slidesPerView={6}
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 22,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+          1280: {
+            slidesPerView: 6,
+            spaceBetween: 32,
+          },
+        }}
+      >
         {categories.map((cat, index) => (
           <SwiperSlide key={cat._id}>
-            <Link to={`/category/${CatName[index]}`} className="geh-category-card">
+            <Link
+              to={`/category/${CatName[index]}`}
+              className="geh-category-card"
+            >
               <div className="geh-image-box">
                 <img src={cat.image} alt={cat.name} />
               </div>
@@ -71,6 +78,3 @@ const CategorySlider = () => {
 };
 
 export default CategorySlider;
-
-
-
