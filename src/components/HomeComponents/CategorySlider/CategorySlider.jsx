94f96a3597fb8b7310dd7478de1233a34b017452
Gehad Sweeney <gehadsowany2001@gmail.com>
2025-11-17 19:50:55 +0200

    update response

--- src/components/HomeComponents/CategorySlider/CategorySlider.jsx
+++ src/components/HomeComponents/CategorySlider/CategorySlider.jsx
@@ -4,9 +4,12 @@ import "swiper/css";
 import { Autoplay } from "swiper/modules";
 import axios from "axios";
 import "./CategorySlider.css";
+import { Link } from "react-router-dom";
 
 const CategorySlider = () => {
   const [categories, setCategories] = useState([]);
+
+  const CatName = ["mobile", "tablet", "smart-watches", "wireless-earbuds", "game-consoles","laptop"];
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -22,26 +25,43 @@ const CategorySlider = () => {
 
   return (
     <div className="geh-category-slider container">
-      <Swiper
-        modules={[Autoplay]}
-        autoplay={{ delay: 2000, disableOnInteraction: false }}
-        spaceBetween={24}
-        slidesPerView={6}
-        breakpoints={{
-          320: { slidesPerView: 2 },
-          480: { slidesPerView: 3 },
-          768: { slidesPerView: 4 },
-          1024: { slidesPerView: 6 },
-        }}
-      >
-        {categories.map((cat) => (
+<Swiper
+  modules={[Autoplay]}
+  autoplay={{ delay: 2000, disableOnInteraction: false }}
+  spaceBetween={32} 
+  slidesPerView={6}
+  breakpoints={{
+    320: { 
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    480: { 
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    768: { 
+      slidesPerView: 4,
+      spaceBetween: 22 
+    },
+    1024: { 
+      slidesPerView: 4,
+      
+    },
+    1280: { 
+      slidesPerView: 6,
+      spaceBetween: 32
+    },
+  }}
+>
+
+        {categories.map((cat, index) => (
           <SwiperSlide key={cat._id}>
-            <a href="#" className="geh-category-card">
+            <Link to={`category/${CatName[index]}`} className="geh-category-card">
               <div className="geh-image-box">
                 <img src={cat.image} alt={cat.name} />
               </div>
               <span className="geh-title">{cat.name}</span>
-            </a>
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
