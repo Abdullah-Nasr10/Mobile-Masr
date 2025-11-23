11724789b614c4b344043acb58dc6b8695715064
Gehad Sweeney <gehadsowany2001@gmail.com>
2025-11-23 22:27:35 +0200

    update design

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,40 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../../store/slices/usersSlice";
-import { IoPersonOutline, IoMailOutline, IoCallOutline } from "react-icons/io5";
-import { RiLockPasswordLine } from "react-icons/ri";
 import "../LoginForm/LoginForm.css";
 import "../../Pages/Auth/Register/Register.css";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { MdOutlineFacebook } from "react-icons/md";
+import { FcGoogle } from "react-icons/fc";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function Register() {
   const dispatch = useDispatch();
   const { loading, error, user, success } = useSelector((s) => s.users);
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
     handleSubmit,
     watch,
     reset,
-    formState: { errors, isSubmitting },
-  } = useForm({ mode: "onBlur" });
+    formState: { errors, isSubmitting, isValid },
+  } = useForm({ mode: "onChange" });
 
   useEffect(() => {
     if (user) {
-      window.location.href = "/"; 
+      window.location.href = "/";
       reset();
     }
   }, [user, reset]);
 
+  useEffect(() => {
+    if (error) {
+      const msg = typeof error === "string" ? error : "Registration failed";
+      toast.error(msg);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (success) {
+      const msg =
+        typeof success === "string" ? success : "Registration successful";
+      toast.success(msg);
+    }
+  }, [success]);
+
   const onSubmit = async (data) => {
     const payload = {
-    name: `${data.firstName} ${data.lastName}`, 
-    email: data.email,
-    password: data.password,
-    phone: data.phone,
-    address: data.address,
-  };
+      name: `${data.firstName} ${data.lastName}`,
+      email: data.email,
+      password: data.password,
+      phone: data.phone,
+      address: data.address,
+    };
     dispatch(registerUser(payload));
   };
 
@@ -42,121 +60,237 @@ export default function Register() {
     <div className="login-center">
       <div className="register-card">
         <div className="text-center mb-5">
-        <h2 className="mb-3">Sign Up</h2>
+          <h2 className="mb-3">Sign Up</h2>
         </div>
 
-        {error && <div className="alert alert-danger">{error}</div>}
-        {success && <div className="alert alert-success">{success}</div>}
-
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="row g-2">
+
+          <div className="row g-4">
             <div className="col-6">
               <div className="mb-3 position-relative">
+                {/* ======firstName===== */}
                 <label className="form-label">First Name</label>
-                <span className="login-input-icon"><IoPersonOutline size={18} /></span>
                 <input
-                  className="form-control geh-form-control ps-4"
+                  className={`form-control geh-form-control ps-4 ${
+                    errors.firstName ? "is-invalid" : ""
+                  }`}
+                  placeholder="First name"
                   {...register("firstName", {
                     required: "First name required",
                     minLength: { value: 3, message: "Min 3 chars" },
+                    maxLength: { value: 10, message: "Max 10 characters" },
+                    pattern: { value: /^\S+$/, message: "No spaces allowed" },
                   })}
                 />
-                {errors.firstName && <small className="text-danger">{errors.firstName.message}</small>}
+                {errors.firstName && (
+                  <small className="text-danger">
+                    {errors.firstName.message}
+                  </small>
+                )}
               </div>
             </div>
 
             <div className="col-6">
               <div className="mb-3 position-relative">
+                {/* ======lastName===== */}
                 <label className="form-label">Last Name</label>
-                <span className="login-input-icon"><IoPersonOutline size={18} /></span>
                 <input
-                  className="form-control geh-form-control ps-4"
+                  className={`form-control geh-form-control ps-4 ${
+                    errors.lastName ? "is-invalid" : ""
+                  }`}
+                  placeholder="Last name"
                   {...register("lastName", {
-                    required: "Last name required",
-                    minLength: { value: 3, message: "Min 3 chars" },
+                      required: "Last name required",
+                      minLength: { value: 3, message: "Min 3 chars" },
+                      maxLength: { value: 10, message: "Max 10 characters" },
+                      pattern: { value: /^\S+$/, message: "No spaces allowed" },
                   })}
                 />
-                {errors.lastName && <small className="text-danger">{errors.lastName.message}</small>}
+                {errors.lastName && (
+                  <small className="text-danger">
+                    {errors.lastName.message}
+                  </small>
+                )}
               </div>
             </div>
           </div>
 
           <div className="mb-4">
+            {/* ======email===== */}
             <label className="form-label">Email</label>
-            <span className="login-input-icon"><IoMailOutline size={18} /></span>
             <input
               type="email"
-              className="form-control geh-form-control ps-4"
+              className={`form-control geh-form-control ps-4 ${
+                errors.email ? "is-invalid" : ""
+              }`}
+              placeholder="you@example.com"
               {...register("email", {
                 required: "Email required",
-                pattern: { value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, message: "Invalid email" },
+                pattern: {
+                  value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                  message: "Invalid email",
+                },
               })}
             />
-            {errors.email && <small className="text-danger">{errors.email.message}</small>}
+            {errors.email && (
+              <small className="text-danger">{errors.email.message}</small>
+            )}
           </div>
 
-          <div className="mb-4">
+          <div className="row g-4">
+                      <div className="mb-4 position-relative col-6">
             <label className="form-label">Password</label>
-            <span className="login-input-icon"><RiLockPasswordLine size={18} /></span>
             <input
-              type="password"
-              className="form-control geh-form-control ps-4"
+              type={showPassword ? "text" : "password"}
+              className={`form-control geh-form-control ps-4 pe-5 ${
+                errors.password ? "is-invalid" : ""
+              }`}
+              placeholder="Create a password"
               {...register("password", {
                 required: "Password required",
+                minLength: { value: 6, message: "Min 6 chars or numbers" },
               })}
             />
-            {errors.password && <small className="text-danger">{errors.password.message}</small>}
+
+            {/* Eye button */}
+            <span
+              className="gehR-password-toggle-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+
+            {errors.password && (
+              <small className="text-danger">{errors.password.message}</small>
+            )}
           </div>
 
-          <div className="mb-4">
+          <div className="mb-4 col-6">
+            {/* ======confirmPassword===== */}
             <label className="form-label">Confirm Password</label>
-            <span className="login-input-icon"><RiLockPasswordLine size={18} /></span>
             <input
               type="password"
-              className="form-control geh-form-control ps-4"
+              className={`form-control geh-form-control ps-4 ${
+                errors.confirmPassword ? "is-invalid" : ""
+              }`}
+              placeholder="Confirm your password"
               {...register("confirmPassword", {
                 required: "Confirm required",
-                validate: (v) => v === watch("password") || "Passwords do not match",
+                validate: (v) =>
+                  v === watch("password") || "Passwords do not match",
               })}
             />
-            {errors.confirmPassword && <small className="text-danger">{errors.confirmPassword.message}</small>}
+            {errors.confirmPassword && (
+              <small className="text-danger">
+                {errors.confirmPassword.message}
+              </small>
+            )}
           </div>
 
-          <div className="mb-5">
+          </div>
+
+
+
+          <div className="mb-3">
+            {/* ======phone===== */}
             <label className="form-label">Phone (optional)</label>
-            <span className="login-input-icon"><IoCallOutline size={18} /></span>
             <input
               type="tel"
-              className="form-control geh-form-control ps-4 "
+              className={`form-control geh-form-control ps-4 ${
+                errors.phone ? "is-invalid" : ""
+              }`}
               placeholder="01XXXXXXXXX"
               {...register("phone", {
-                pattern: { value: /^01[0-2,5]{1}[0-9]{8}$/, message: "Invalid phone" },
+                pattern: {
+                  value: /^01[0-2,5]{1}[0-9]{8}$/,
+                  message: "Phone must be valid",
+                },
               })}
             />
-            {errors.phone && <small className="text-danger">{errors.phone.message}</small>}
+            {errors.phone && (
+              <small className="text-danger">{errors.phone.message}</small>
+            )}
+          </div>
+          {/* ===== Terms & Newsletter ===== */}
+          <div className="mb-3">
+            {/* Terms (Required) */}
+            <div className="form-check mb-2">
+              <input
+                type="checkbox"
+                className={`form-check-input ${
+                  errors.terms ? "is-invalid" : ""
+                }`}
+                id="terms"
+                {...register("terms", {
+                  required: "Acceptance of terms and conditions is required",
+                })}
+              />
+              <label className="form-check-label" htmlFor="terms">
+                <small>
+                  I agree to the <strong>Terms & Conditions</strong> and the{" "}
+                  <strong>Privacy Notice</strong> of MobileMasr.
+                </small>
+              </label>
+            </div>
+
+            {errors.terms && (
+              <small className="text-danger">{errors.terms.message}</small>
+            )}
+
+            {/* Newsletter (Optional) */}
+            <div className="form-check mt-3">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="newsletter"
+                {...register("newsletter")}
+              />
+              <label className="form-check-label" htmlFor="newsletter">
+                <small>
+                  I want to receive MobileMasr newsletter and best offers.
+                </small>
+              </label>
+            </div>
           </div>
 
-          <button type="submit" className="btn btn-geh w-100 mb-5" disabled={isSubmitting || loading}>
-            {loading ? "Registering..." : "Register"}
-          </button>
+          {/* =======button====== */}
+          <div>
+            <button
+              type="submit"
+              className="btn btn-geh w-100 mb-5"
+              disabled={isSubmitting || loading || !isValid}
+              aria-disabled={isSubmitting || loading || !isValid}
+            >
+              {loading
+                ? "Registering..."
+                : isSubmitting
+                ? "Submitting..."
+                : "Register"}
+            </button>
+          </div>
         </form>
-              <div className="text-center mt-4">
-        <span className="text-muted">Or</span>
-      </div>
+        <div className="text-center">
+          <span className="text-muted">Or</span>
+        </div>
 
-      <div className="login-socials">
-        <Link to="#">
-          <img src="https://res.cloudinary.com/dfigu6nnn/image/upload/v1763739706/icons8-google-48_oqslny.png" alt="Google" />
-        </Link>
-        <Link to="#">
-          <img src="https://res.cloudinary.com/dfigu6nnn/image/upload/v1763739873/icons8-facebook-logo-48_lgpr9x.png" alt="Facebook" />
-        </Link>
-      </div>
-      <div className="text-center mt-4">
-        <p className="text-muted">Already have an account?<Link to="/login" className=" geh-link text-decoration-none p-2">Sign In</Link></p>
-      </div>
+        <div className="login-socials">
+          <Link to="#">
+            <FcGoogle />
+          </Link>
+          <Link to="#">
+            <MdOutlineFacebook />
+          </Link>
+        </div>
+
+        <div className="text-center mt-4">
+          <p className="text-muted">
+            Already have an account?
+            <Link to="/login" className=" geh-link text-decoration-none p-2">
+              Sign In
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
