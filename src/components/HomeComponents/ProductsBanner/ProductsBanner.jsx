6ac7d88567e26ee8ad89657ce077f5064c841e5a
Mustafa-Mahmoud-Ahmed <moustafa.mahnoud2024@gmail.com>
2025-12-08 15:37:39 +0200

    mos: update favourite and update Home

---
+++ src/components/HomeComponents/ProductsBanner/ProductsBanner.jsx
@@ -0,0 +1,18 @@
+import "./ProductsBanner.css";
+
+function ProductsBanners() {
+  return (
+    <div className="mos-products-banners container my-5">
+      <div className="mos-products-banner-grid">
+
+        <img src="/Images/iphone15.webp" alt="Apple Banner" />
+        <img src="/Images/iphone15promax.webp" alt="Samsung Banner" />
+        <img src="/Images/iphone16pro.webp" alt="Xiaomi Banner" />
+        <img src="/Images/iphone16promax.webp" alt="Oppo Banner" />
+
+      </div>
+    </div>
+  );
+}
+
+export default ProductsBanners;
