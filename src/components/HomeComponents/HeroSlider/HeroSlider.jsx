1fb2e7260e3c4affc2d7a5c74cc61e53a773975f
Abdullah Nasr <abdullah.nasr1022@gmail.com>
2025-12-31 23:00:48 +0200

    abd: translate photo in slider

--- src/components/HomeComponents/HeroSlider/HeroSlider.jsx
+++ src/components/HomeComponents/HeroSlider/HeroSlider.jsx
@@ -5,31 +5,33 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "./HeroSlider.css";
+import { useSelector } from "react-redux";
 
 const slides = [
   {
-    imageUrl:
-      "https://res.cloudinary.com/dj1omur11/image/upload/v1766781700/HeroSlider1_njn5af.jpg",
+    en: "https://res.cloudinary.com/dj1omur11/image/upload/v1766781700/HeroSlider1_njn5af.jpg",
+    ar: "https://res.cloudinary.com/dj1omur11/image/upload/v1766781700/HeroSlider1_njn5af.jpg", // عدل هذا لاحقًا
   },
   {
-    imageUrl:
-      "https://res.cloudinary.com/dj1omur11/image/upload/v1766777928/HeroSlider2.1_iarmdz.png",
+    en: "https://res.cloudinary.com/dj1omur11/image/upload/v1766777928/HeroSlider2.1_iarmdz.png",
+    ar: "https://res.cloudinary.com/dj1omur11/image/upload/v1767214641/HeroSlider2.2_1_evei64.png", // عدل هذا لاحقًا
   },
   {
-    imageUrl:
-      "https://res.cloudinary.com/dj1omur11/image/upload/v1766780241/HeroSlider3.1_knaqm9.jpg",
+    en: "https://res.cloudinary.com/dj1omur11/image/upload/v1766780241/HeroSlider3.1_knaqm9.jpg",
+    ar: "https://res.cloudinary.com/dj1omur11/image/upload/v1766780241/HeroSlider3.1_knaqm9.jpg", // عدل هذا لاحقًا
   },
   {
-    imageUrl:
-      "https://res.cloudinary.com/dfigu6nnn/image/upload/v1762698939/5_xfim4d.webp",
+    en: "https://res.cloudinary.com/dfigu6nnn/image/upload/v1762698939/5_xfim4d.webp",
+    ar: "https://res.cloudinary.com/dfigu6nnn/image/upload/v1762698939/5_xfim4d.webp", // عدل هذا لاحقًا
   },
   {
-    imageUrl:
-      "https://res.cloudinary.com/dfigu6nnn/image/upload/v1762698939/3_sry7wf.webp",
+    en: "https://res.cloudinary.com/dfigu6nnn/image/upload/v1762698939/3_sry7wf.webp",
+    ar: "https://res.cloudinary.com/dfigu6nnn/image/upload/v1762698939/3_sry7wf.webp", // عدل هذا لاحقًا
   },
 ];
 
 const HeroSlider = () => {
+  const currentLang = useSelector((state) => state.language.currentLang);
   return (
     <div className="geh-hero-slider">
       <div className="container">
@@ -48,7 +50,11 @@ const HeroSlider = () => {
           {slides.map((slide, index) => (
             <SwiperSlide key={index}>
               <div className="geh-slide">
-                <img src={slide.imageUrl} alt="" className="img-fluid" />
+                <img
+                  src={slide[currentLang] || slide.en}
+                  alt=""
+                  className="img-fluid"
+                />
               </div>
             </SwiperSlide>
           ))}
